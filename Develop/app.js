b2859fc7cd084ebe8dd2b04d72ba92e64368d951
fruityvegetables <gg.troyer@gmail.com>
2020-04-20 14:06:40 -0500

    LOL there was a zero-space white UTF character making my program INOPERABLE

--- Develop/app.js
+++ Develop/app.js
@@ -4,18 +4,9 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
 const render = require("./lib/htmlRenderer");
-
-​//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-​//functions~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 function startPrompt(){
     //prompt the user with what kind of employee to create
     return inquirer
@@ -38,7 +29,6 @@ function startPrompt(){
     //     return;
     // }
 }
-
 function createManager(){
     return inquirer
     .prompt([
@@ -70,7 +60,6 @@ function createManager(){
     ])
 
 };
-
 function createEngineer(){
     return inquirer
     .prompt([
@@ -102,7 +91,6 @@ function createEngineer(){
     ])
 
 };
-
 function createIntern(){
     return inquirer
     .prompt([
@@ -134,7 +122,6 @@ function createIntern(){
     ])
 
 };
-
 function againPrompt(){
     return inquirer.prompt([
         {
@@ -146,7 +133,6 @@ function againPrompt(){
 
     ]);
 }
-
 async function startApp(){
     const employees = [];
     let firstStart = true;
@@ -157,9 +143,7 @@ async function startApp(){
             firstStart = false;
             type = {role:"Manager"}
         }
-
         let data = await createManager();
-
         switch(type.role){
             case 'Manager':
                 employees.push(new Manager(data.name, data.id, data.email, data.github, data.imgUrl));
@@ -171,7 +155,6 @@ async function startApp(){
                 employees.push(new Intern(data.name, data.id, data.email, data.school, data.imgUrl));
                 break; 
         }
-
         var createMore = await againPrompt();
     } while (createMore.confirm!= "No");
     const html = render(employees);
@@ -191,30 +174,24 @@ async function startApp(){
         console.log("Wrote to a file called team.html!");
     })
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 startApp();
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-​
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-​
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-​
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an 
